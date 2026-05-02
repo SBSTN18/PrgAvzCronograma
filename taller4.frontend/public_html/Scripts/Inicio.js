baeeed27d8ceee8310ed27291e41922bd6c9aaff
SBSTN18 <sebastian.zambranoh18@gmail.com>
2026-05-02 02:16:05 -0500

    ya entendi esto por fin

---
+++ taller4.frontend/public_html/Scripts/Inicio.js
@@ -0,0 +1,15 @@
+document.getElementById("inicios").addEventListener("click", () => {
+  window.location.href = "Login.html";
+});
+
+document.getElementById("registro").addEventListener("click", () => {
+  window.location.href = "Registrar.html";
+});
+
+document.getElementById("nuevahijo").addEventListener("click", () => {
+  window.location.href = "Registrar.html";
+}); 
+
+document.getElementById("nuevatutor").addEventListener("click", () => {
+  window.location.href = "Registrar.html";
+});
---
+++ taller4.frontend/public_html/Scripts/Login.js
@@ -0,0 +1,5 @@
+document.getElementById("loginbtn").addEventListener("click", function(event) {
+    event.preventDefault();
+    window.location.href = "Principal.html";
+    
+});
